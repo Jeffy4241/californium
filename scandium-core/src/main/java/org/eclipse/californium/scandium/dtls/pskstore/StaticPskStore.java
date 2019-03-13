4b7c161fbebde056ec0a7423fe60c2388d032131
Achim Kraus <achim.kraus@bosch-si.com>
2019-03-13 12:36:39 +0100

    Introduce raw byte arrays for psk hint and identity.
    Support PSK clients with non-compliant encoding.
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/pskstore/StaticPskStore.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/pskstore/StaticPskStore.java
@@ -19,21 +19,22 @@ package org.eclipse.californium.scandium.dtls.pskstore;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
 
+import org.eclipse.californium.scandium.dtls.PskPublicInformation;
 import org.eclipse.californium.scandium.util.ServerNames;
 
 /**
  * A simple in-memory pre-shared-key store.
  * <p>
- * This implementation always returns the same identity/Key for all peers
- * and is mainly intended for testing and evaluation purposes.
+ * This implementation always returns the same identity/Key for all peers and is
+ * mainly intended for testing and evaluation purposes.
  * <p>
  * NB Keeping keys in in-memory is not a good idea for production. Instead, keys
  * should be kept in an encrypted store.
  */
-public class StaticPskStore implements PskStore {
+public class StaticPskStore implements BytesPskStore {
 
 	private final byte[] key;
-	private final String fixedIdentity;
+	private final PskPublicInformation fixedIdentity;
 
 	/**
 	 * Creates a new store for an identity and key.
@@ -42,27 +43,43 @@ public class StaticPskStore implements PskStore {
 	 * @param key The (single) key for the identity.
 	 */
 	public StaticPskStore(final String identity, final byte[] key) {
+		this(new PskPublicInformation(identity), key);
+	}
+
+	/**
+	 * Creates a new store for an identity and key.
+	 * 
+	 * @param identity The (single) identity to always use.
+	 * @param key The (single) key for the identity.
+	 */
+	public StaticPskStore(final PskPublicInformation identity, final byte[] key) {
 		this.fixedIdentity = identity;
 		this.key = Arrays.copyOf(key, key.length);
 	}
 
 	@Override
-	public String getIdentity(final InetSocketAddress inetAddress) {
+	public PskPublicInformation getIdentity(final InetSocketAddress inetAddress) {
 		return fixedIdentity;
 	}
 
 	@Override
-	public String getIdentity(InetSocketAddress peerAddress, ServerNames virtualHost) {
+	public PskPublicInformation getIdentity(InetSocketAddress peerAddress, ServerNames virtualHost) {
 		return getIdentity(peerAddress);
 	}
 
 	@Override
-	public byte[] getKey(final String identity) {
+	public byte[] getKey(final PskPublicInformation identity) {
+		if (!fixedIdentity.equals(identity)) {
+			return null;
+		}
+		if (!fixedIdentity.isCompliantEncoding()) {
+			identity.normalize(fixedIdentity.getPublicInfoAsString());
+		}
 		return key;
 	}
 
 	@Override
-	public byte[] getKey(final ServerNames serverNames, final String identity) {
+	public byte[] getKey(final ServerNames serverNames, final PskPublicInformation identity) {
 		return getKey(identity);
 	}
 }
